cd0fe57fca5cc4bca77b16d028a522badd8f5423
Aditi-Agale <aditiagale1803@gmail.com>
2025-05-27 14:34:53 +0530

    Changes Updated

--- backend/app.py
+++ backend/app.py
@@ -177,4 +177,5 @@ def today_reminders():
 # ---------------------- RUN APP ----------------------
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host='0.0.0.0', port=port)
